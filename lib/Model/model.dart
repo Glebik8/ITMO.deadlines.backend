f812fa4c27b4c0b1c601e54fc558859e939814de
Андрей Морозов <ezforlive2@gmail.com>
2020-09-21 12:29:58 +0300

    starting learn hive - completed screen divide

---
+++ lib/Model/model.dart
@@ -0,0 +1,24 @@
+
+
+import 'dart:ui';
+import 'package:hive/hive.dart';
+
+
+class Model{
+
+  Color headColor = Color(0xFF8D93AB);
+  Color bodyColor = Color(0xFFFFFFFF);
+
+  void initHive() {
+
+//    var path = Directory.current.path;
+//    Hive
+//      ..init(path)
+//      ..registerAdapter(PersonAdapter());
+//
+//    var box = Hive.box('myBox');
+//    box.put('name', 'David');
+//    var name = box.get('name');
+//    print('Name: $name');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
 
+import 'Model/model.dart';
+
+
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Model model = Model();
+  @override
+  void initState() {
+    model.initHive();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold()
+
+      home: Scaffold(
+        body: Container(
+          height: double.infinity,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                color: model.headColor,
+                width: double.infinity,
+                height: 100.0,
+                child: chooseList(context)
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: Container(
+                  color: model.bodyColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget chooseList(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Text(
+          "Мои дедлайны",
+          style: TextStyle(color: Colors.white, fontSize: 25),
+        ),),
     );
   }
 }
